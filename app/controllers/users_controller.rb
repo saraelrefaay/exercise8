d29b987e59f6ebfd5bd4c6e51a10ae04ea537b66
saraelrefaay <sara.o.elrefaay@gmail.com>
2015-08-03 23:08:28 +0200

    complete exercise 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
 
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
+
   def new
   	@user = User.new
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def admin_user
+      redirect_to(root_path) unless current_user.admin?
   end
 
   def show
@@ -26,10 +38,34 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      #updates
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
     else
       render 'edit'
     end
   end
+
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
+  private
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+
+
   
 end
